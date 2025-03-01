39e242e9564bd5b4e9aca4d94146f5b4d9df6141
ozkan <ozkandnz09@gmail.com>
2025-03-01 21:25:39 +0300

    firm model

--- src/models/firm.js
+++ src/models/firm.js
@@ -1,6 +1,35 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
 /* ------------------------------------------------------- */
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+    },
+    phone: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    address: {
+      type: String,
+      trim: true,
+    },
+    image: {
+      type: String,
+      trim: true,
+    },
+  },
+  { collection: "users", timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);
