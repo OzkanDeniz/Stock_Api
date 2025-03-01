c78ac66b5bd42325890d071927737e19d8ddae45
ozkan <ozkandnz09@gmail.com>
2025-03-01 20:08:19 +0300

    router

--- src/routes/user.js
+++ src/routes/user.js
@@ -13,4 +13,5 @@ router.route("/").get(list).post(create)
 
 router.route("/:id").get(read).put(update).patch(update).delete(deletee)
 
+/* ------------------------------------------------------- */
 module.exports = router
