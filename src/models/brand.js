e16747a9ebed7fada95200aa775afd70cbc78c28
ozkan <ozkandnz09@gmail.com>
2025-03-01 21:40:59 +0300

    brands model

--- src/models/brand.js
+++ src/models/brand.js
@@ -1,6 +1,25 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
+
 /* ------------------------------------------------------- */
+
+const BrandSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+    },
+    image: {
+      type: String,
+      trim: true,
+    },
+  },
+  { collection: "brands", timestamps: true }
+);
+
+module.exports = mongoose.model("Brand", BrandSchema);
