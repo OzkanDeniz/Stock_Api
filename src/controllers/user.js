045d732fc3998d5b8e566147048ca3b3dc78c1a7
ozkan <ozkandnz09@gmail.com>
2025-03-01 19:21:06 +0300

    crud

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -6,8 +6,57 @@ const User = require("../models/user");
 
 module.exports = {
   list: async (req, res) => {
-    const data = await User.find();
+    /* 
+    #swagger.tags = ["Users"]
+    #swagger.summary = "List Users"
+    #swagger.description = `
+                You can send query with endpoint for search[], sort[], page and limit.
+                <ul> Examples:
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                    <li>URL/?<b>page=2&limit=1</b></li>
+                </ul>
     
+     */
+    const data = await User.find();
+
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+
+  //! CRUD:
+  create: async (req, res) => {
+    const data = await User.find();
+
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+
+  read: async (req, res) => {
+    const data = await User.find();
+
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+
+  update: async (req, res) => {
+    const data = await User.find();
+
+    res.status(200).send({
+      error: false,
+      data,
+    });
+  },
+
+  delete: async (req, res) => {
+    const data = await User.find();
+
     res.status(200).send({
       error: false,
       data,
